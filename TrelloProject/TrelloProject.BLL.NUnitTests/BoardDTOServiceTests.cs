4f3f5fd76a3902352b772485755fdbb7b727f49a
Valentina Zaitseva <v.zaitseva@aimprosoft.com>
2019-09-03 10:30:07 +0300

    Unit tests are created for Get() and GetById() methods

---
+++ TrelloProject/TrelloProject.BLL.NUnitTests/BoardDTOServiceTests.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using TrelloProject.BLL.DTO;
+using TrelloProject.BLL.Services;
+using TrelloProject.DAL.Entities;
+
+namespace TrelloProject.BLL.Tests
+{
+    [TestFixture]
+    class BoardDTOServiceTests
+    {
+        [Test]
+        public void GetAllBoardsDTO_ByDefault_ReturnsListOfTypeBoardDTO()
+        {
+            //Arrange
+            var stub = new BoardRepositoryStub();
+            IEnumerable<Board> list = new List<Board> {
+                            new Board { BoardId = 1, Title = "TestBoard1", CurrentBackgroundColorId = 1 },
+                            new Board { BoardId = 2, Title = "TestBoard2", CurrentBackgroundColorId = 1 },
+                            new Board { BoardId = 3, Title = "TestBoard3", CurrentBackgroundColorId = 1 }
+                        };
+
+            stub.SetReturnList(list);
+
+            BoardDTOService boardDTOService = new BoardDTOService(stub);
+
+            //Act
+            var result = boardDTOService.GetAllBoardsDTO();
+
+            //Assert
+            Assert.IsInstanceOf<List<BoardDTO>>(result);
+
+        }
+
+        [Test]
+        public void GetBoardDTO_ByDefault_ReturnsObjectOfTypeBoardDTO()
+        {
+            //Arrange
+            var stub = new BoardRepositoryStub();
+            Board board = new Board { BoardId = 1, Title = "TestBoard1", CurrentBackgroundColorId = 1 };
+
+            stub.SetReturnObject(board);
+
+            BoardDTOService boardDTOService = new BoardDTOService(stub);
+
+            //Act
+            var result = boardDTOService.GetBoardDTO(3);
+
+            //Assert
+            Assert.IsInstanceOf<BoardDTO>(result);
+        }
+    }
+}
---
+++ TrelloProject/TrelloProject.BLL.NUnitTests/BoardRepositoryStub.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using TrelloProject.DAL.Entities;
+using TrelloProject.DAL.Interfaces;
+
+namespace TrelloProject.BLL.Tests
+{
+    class BoardRepositoryStub : IBoardRepository
+    {
+        private IEnumerable<Board> _list;
+        private Board _board;
+
+        public void SetReturnList(IEnumerable<Board> list)
+        {
+            _list = list;
+        }
+
+        public IEnumerable<Board> GetAllBoards()
+        {
+            return _list;
+        }
+
+        public Board GetBoard(int Id)
+        {
+            return _board;
+        }
+
+        public void SetReturnObject(Board board)
+        {
+            _board = board;
+        }
+    }
+}
